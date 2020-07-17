d36de7f8ca691cb11da203e8f0e7c41f76213869
unknown <nagambika.anguraj@gmail.com>
2020-07-17 18:04:09 +0530

    Updates

---
+++ precache-manifest.8ebefe69e9218d9a99529d92b3c26a53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedb02b2f9ea63aeda5d861113818c5a",
+    "url": "/nagambika-anguraj/google-keep.git/index.html"
+  },
+  {
+    "revision": "d1d07b676baebcd72c1a",
+    "url": "/nagambika-anguraj/google-keep.git/static/css/main.8cee7949.chunk.css"
+  },
+  {
+    "revision": "7ba16d68fcb1cef52395",
+    "url": "/nagambika-anguraj/google-keep.git/static/js/2.16becaa1.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/nagambika-anguraj/google-keep.git/static/js/2.16becaa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d07b676baebcd72c1a",
+    "url": "/nagambika-anguraj/google-keep.git/static/js/main.31905cd4.chunk.js"
+  },
+  {
+    "revision": "d62ded4588dbcddcd497",
+    "url": "/nagambika-anguraj/google-keep.git/static/js/runtime-main.683a7952.js"
+  }
+]);
